f1ea827f0059bde1a6ef598c942d6617d25b3079
mmiche01 <mmiche01@students.uni-mainz.de>
2022-10-03 16:12:15 +0200

    Add tooltip

--- public/script.js
+++ public/script.js
@@ -1,15 +1,26 @@
 const btnCopy = document.querySelector('.btn-copy');
+const textArea = document.querySelector('.lorem-ipsum-text');
 
 btnCopy.addEventListener('click', copyToClipboard);
 
 function copyToClipboard() {
-	textToCopy = document.querySelector('.lorem-ipsum-text').innerText;
+	textToCopy = textArea.innerText;
 	navigator.clipboard.writeText(textToCopy).then(
 		() => {
-			alert('Copied');
+			showTooltip();
 		},
 		() => {
 			alert('Copy failed');
 		}
 	);
+}
+
+function showTooltip() {
+	const tooltip = document.createElement('span');
+	tooltip.classList.add('tooltip');
+	tooltip.innerText = 'Copied!';
+	textArea.prepend(tooltip);
+	setTimeout(() => {
+		tooltip.remove();
+	}, 1000);
 }
